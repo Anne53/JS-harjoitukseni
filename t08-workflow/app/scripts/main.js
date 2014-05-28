03125038c52fddc69f9aa5723b6c966e97635e5c
Anne <anne.benson@haaga-helia.fi>
2014-05-28 10:58:23 +0000

    angularia lisää repoon

---
+++ t08-workflow/app/scripts/main.js
@@ -0,0 +1,20 @@
+
+    <script>
+      var myDataRef = new Firebase('https://x6q1725ot0l.firebaseio-demo.com/');
+      $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+          var name = $('#nameInput').val();
+          var text = $('#messageInput').val();
+          myDataRef.push({name: name, text: text});
+          $('#messageInput').val('');
+        }
+      });
+      myDataRef.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+        displayChatMessage(message.name, message.text);
+      });
+      function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      };
+    </script>
